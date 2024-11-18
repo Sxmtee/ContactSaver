197e5434813d427da3d62a67419c2e0452c8292d
sxmtee <somtoo76@gmail.com>
2024-11-18 08:47:56 +0100

    Initial Commit

---
+++ ContactSaver/ContactSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactSaverApp.swift
+//  ContactSaver
+//
+//  Created by mac on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
